6a66c0015bbbe0103efa60417c685b22ce13bf37
Paloma Souza <paloma.souza@gmail.com>
2024-05-22 11:48:03 -0300

    add logica aula

---
+++ 01_Logica/01_Logica/Program.cs
@@ -0,0 +1,58 @@
+﻿internal class Program
+{
+    internal class Funcionario
+    {
+        public string Nome { get; set; }
+        public int ExperienciaProfissional { get; set; }
+    }
+
+    private static void Main(string[] args)
+    {
+        // tipo referncia e tipo valor
+        int idade = 20;
+
+        int idade2 = idade;
+        idade2 = 30;
+
+        Console.WriteLine("idade = " + idade + " idade2 = " + idade2); // idade= 20 idade2= 30
+
+        Funcionario funcionario = new Funcionario() { Nome = "Paulo", ExperienciaProfissional = 2 };
+        Funcionario funcionario2 = funcionario;
+
+        funcionario2.ExperienciaProfissional += 1;
+
+        Console.WriteLine("Funcionário 1: " + funcionario2.ExperienciaProfissional
+        + " funcionário2 :" + funcionario2.ExperienciaProfissional); // Funcionário 1: 3 funcionário2 :3
+
+        //garbage collector
+        //TODO
+
+        // tipos em c #
+        char sexo = 'F';
+        bool feliz = false;
+        DateTime date = DateTime.Now;
+        //TODO: outros tipos
+
+        // cast
+
+        Console.WriteLine("Felling good?");
+
+        bool resposta = Convert.ToBoolean(Console.ReadLine());
+        if (resposta)
+        {
+            Console.WriteLine("I feel amazing");
+        }
+        else
+        {
+            Console.WriteLine("Sorry");
+        }
+
+        // interpolação de strings
+        string valor = string.Format("minha idade é {0}", idade);
+        string str = $"Hello {funcionario.Nome}. Today is {date}.";
+
+        Console.WriteLine($"{valor}\n{str}");
+
+        Console.ReadKey();
+    }
+}
---
+++ 01_Logica/Exercicio01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Um código onde voce pergunta para o usuário o nome dele, a idade, o cargo
+// e no final imprime uma saudação na tela com todas as infos e com data e hora
+
+Console.WriteLine("Qual o seu nome, idade e cargo? Digite e separe apenas por virgulas");
+
+string resposta = Console.ReadLine();
+string[] usuario = resposta.Split(",");
+
+string name = usuario[0];
+int idade = int.Parse(usuario[1]);
+string cargo = usuario[2];
+
+Console.WriteLine($"Olá, {name}! Você tem {idade} anos e ocupa o cargo de {cargo}. Hora atual: {DateTime.Now}");
